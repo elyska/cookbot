ec8a3e6c4a1a035dd7c29d42e4dcf93a9cbc4217
ryklovae <ryklovae@coventry.ac.uk>
2020-11-23 17:55:04 +0000

    test processUserMessage

--- UnitTesting.py
+++ UnitTesting.py
@@ -13,17 +13,17 @@ class TestBot(unittest.TestCase):
     #    self.assertNotIn('recipe',dummyAccessApi()['hits'][0])
 
     # meaningful input, recipes found
-    #def testAccessApi1(self):
-        #self.assertIn('recipe',accessApi('pizza', ['vegetarian'], [])['hits'][0])
+    def testAccessApi1(self):
+        self.assertIn('recipe',accessApi('pizza', ['vegetarian'], [])['hits'][0])
     # meaningless input, no recipes found
-    #def testAccessApi2(self):
-        #self.assertEqual(0,len(accessApi('hdfhhbdjhrw', [], [])['hits']))
+    def testAccessApi2(self):
+        self.assertEqual(0,len(accessApi('hdfhhbdjhrw', [], [])['hits']))
     # multiple filters
-    #def testAccessApi3(self):
-        #apiCall = accessApi('pizza', ['vegetarian', 'vegan'], ['low-fat'])
-        #self.assertIn('Vegan',apiCall['hits'][0]['recipe']['healthLabels'])
-        #self.assertIn('Vegetarian', apiCall['hits'][0]['recipe']['healthLabels'])
-        #self.assertIn('Low-Fat', apiCall['hits'][0]['recipe']['dietLabels'])
+    def testAccessApi3(self):
+        apiCall = accessApi('pizza', ['vegetarian', 'vegan'], ['low-fat'])
+        self.assertIn('Vegan',apiCall['hits'][0]['recipe']['healthLabels'])
+        self.assertIn('Vegetarian', apiCall['hits'][0]['recipe']['healthLabels'])
+        self.assertIn('Low-Fat', apiCall['hits'][0]['recipe']['dietLabels'])
 
     def testSeparateContractions(self):
         self.assertIn("n't",separateContractions(["i", "don't", "like", "tomatoes"]))
@@ -34,13 +34,8 @@ class TestBot(unittest.TestCase):
         self.assertEqual(["pizza","peanuts"], meaningfulWords("I'd like to make a pizza. I am allergic to peanuts"))
         self.assertEqual(["apple", "tomatoes"], meaningfulWords("I've got an apple. I don't have tomatoes."))
 
-
-#x = "I don't like tomatoes."
-#y = "I'd like to make a pizza. I am allergic to peanuts"
-#z = "I've got an apple. I don't have tomatoes."
-#print(meaningfulWords(x))
-#print(meaningfulWords(y))
-#print(meaningfulWords(z))
+    def testProcessUserMessage(self):
+        self.assertEqual("hola vsssgr jzrfasdf skirj",processUserMessage("Hola! (vsss)gr JZRFa<sdf: ski'rj."))
 
 
 
